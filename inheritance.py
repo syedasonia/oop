ea9b4a96cb59df9e97a7917a70485791bd9cbfeb
syedasonia <syedasoniamurad@gmail.com>
2021-10-05 11:40:00 +0100

    with inherit

---
+++ inheritance.py
@@ -0,0 +1,42 @@
+# lecture of inheritance 
+#resturant manu 
+
+class restaurantmenu:
+    def __init__(self,name, price,ingredients):
+        self.name = name
+        self.price = price
+        self.ingredients = ingredients   
+class menucatagories(restaurantmenu):
+    def __init__(self, name, price, catagories,ingredients):
+        super().__init__(name, price,ingredients)
+        self.catagories= catagories
+
+class drink(restaurantmenu):
+    def __init__(self, name, price, flavors ,ingredients):
+        super().__init__(name, price,ingredients)
+        self.flavors = flavors
+    
+class starters(menucatagories):
+        def __init__(self, name, price, catagories ,ingredients):
+            super().__init__(name, price, catagories ,ingredients)
+            
+
+class mainmenu(menucatagories):
+    def __init__(self, name, price, catagories ,ingredients):
+        super().__init__(name, price, catagories ,ingredients)
+        
+
+ 
+class dessert(menucatagories):
+    def __init__(self, name, price, catagories ,ingredients):
+        super().__init__(name, price, catagories ,ingredients)
+       
+food1 = drink("coke","£2","No-suger","Non-Alcoholic")
+food2 = starters("Dimsum","£10","chinese","chicken")
+food3 = starters("cuisines","£15","indian","lam")
+food4 = dessert("pie","£1","english","egg")
+
+print(food1.price)
+print(food2.name)
+print(food3.catagories)
+print(food4.name,food4.price , food4.catagories ,food4.ingredients)

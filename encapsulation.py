9653fc0f742b9bc384044a3584bca2da80c9da13
syedasonia <syedasoniamurad@gmail.com>
2021-10-30 23:11:07 +0100

    edit made

--- encapsulation.py
+++ encapsulation.py
@@ -11,12 +11,14 @@ class CheddarMultiflvourpack():
 
 cheddarcatagorione = CheddarMultiflvourpack('BBQ', 15) 
 cheddarcatagoritwo = CheddarMultiflvourpack('chilli', 15)
+cheddarcatagorithree = CheddarMultiflvourpack('original', 15)
 #accessing using class method
 cheddarcatagorione.display() 
 cheddarcatagoritwo.display()
 #accessing directly from outside
 print(cheddarcatagorione.flavour,cheddarcatagorione.weight)  
-print(cheddarcatagoritwo.flavour,cheddarcatagoritwo.weight)    
+print(cheddarcatagoritwo.flavour,cheddarcatagoritwo.weight) 
+print(cheddarcatagorithree.flavour,cheddarcatagorithree.weight)    
 """
 # a simple class with some attibutes and it's instances
 

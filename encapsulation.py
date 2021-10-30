2d212010a52d0bcc7eb4775d3b2e5a029eed8203
syedasonia <syedasoniamurad@gmail.com>
2021-10-30 23:05:13 +0100

    after using encapsulation

--- encapsulation.py
+++ encapsulation.py
@@ -1,8 +1,5 @@
 # example of encapsulation 
-
-
-# a simple class with some attibutes and it's instances
-
+"""
 class CheddarMultiflvourpack():
     def __init__(self,flavour,weight):
         self.flavour = flavour
@@ -20,3 +17,41 @@ cheddarcatagoritwo.display()
 #accessing directly from outside
 print(cheddarcatagorione.flavour,cheddarcatagorione.weight)  
 print(cheddarcatagoritwo.flavour,cheddarcatagoritwo.weight)    
+"""
+# a simple class with some attibutes and it's instances
+
+class CheddarMultiflvourpack():
+    def __init__(self,flavour,weight):
+        self.flavour = flavour
+        self.weight = weight
+
+    def display(self):
+          print(self.flavour)
+          print(self.weight)
+
+class Cheddarcatagoritwo(CheddarMultiflvourpack): 
+    def __init__(self, flavour, weight):
+        super().__init__(flavour, weight)  
+
+class Cheddarcatagorithree(CheddarMultiflvourpack):
+    def __init__(self, __flavour, __weight):
+        super().__init__(__flavour, __weight)  
+
+    def dontdisplay(self):
+          print(self.__flavour)
+          print(self.__weight)
+    
+
+cheddarcatagorione = CheddarMultiflvourpack('BBQ', 15)
+cheddarcatagoritwo = Cheddarcatagoritwo('chilli', 15) 
+cheddarcatagorithree = Cheddarcatagorithree('original', 15)
+#accessing using class method
+
+cheddarcatagorione.display()
+cheddarcatagoritwo.display()
+cheddarcatagorithree.dontdisplay()
+
+#accessing directly from outside
+print(cheddarcatagorione.flavour,cheddarcatagorione.weight) 
+print(cheddarcatagoritwo.flavour,cheddarcatagoritwo.weight) 
+print(cheddarcatagorithree.__flavour,cheddarcatagorithree.__weight)
